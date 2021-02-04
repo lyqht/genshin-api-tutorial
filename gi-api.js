2d8930eb92a6b39ca35c9a824418e942a89313be
lyqht <estee.tsw@gmail.com>
2021-02-04 22:10:33 +0800

    completed simple website using genshin impact fanmade api

---
+++ gi-api.js
@@ -0,0 +1,29 @@
+function getAllCharacters() {
+    fetch("https://api.genshin.dev/characters")
+    .then((response) => response.json()
+    .then((data)=> {
+        console.log({data});
+        const select = document.getElementById("characters");
+        for (const character of data) {
+            var option = document.createElement("option");
+            option.text = character;
+            option.value = character;
+            select.add(option);
+        }
+    }))
+}
+function getCharacterInfo(character) {
+    fetch("https://api.genshin.dev/characters/" + character)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log({data});
+        document.getElementById("character-image").src = "https://api.genshin.dev/characters/" + character + "/icon";
+        const thingsToSet = ['name', 'rarity', 'vision', 'weapon'];
+        for (const property of thingsToSet) {
+            const text = document.getElementById(property);
+            text.textContent = data[property];
+        }
+    })
+}
+
+getAllCharacters();
